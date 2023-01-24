c1955907307e027a8d4e00d0adddb8727b87e8d9
krishpranav <krisna.pranav@gmail.com>
2023-01-24 19:26:37 +0530

    Initial Commit

---
+++ web-server/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  web-server
+//
+//  Created by krisna pranav on 24/01/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
